2503d8c169ad9fa3ac35612d17458f775f5878f5
canontank <58597814+canontank@users.noreply.github.com>
2023-09-18 19:32:58 +0900

    Update cookie.js

--- cookie/js/cookie.js
+++ cookie/js/cookie.js
@@ -220,7 +220,7 @@ function getDiscount(data) {
 }
 
 function getAddDiscount(data) {
-    if (cardArray1.indexOf(data[1]) != -1 && data[3] == "간편결제" && (data[5] * data[6]) >= 10000) {
+    if ((cardArray1.indexOf(data[1]) != -1) && data[3] == "간편결제" && (data[5] * data[6]) >= 10000) {
         return 1000;
     }
 	return 0;
