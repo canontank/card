287201c9e3a4314f5a2ad76a26d4ef3c5c8ef058
canontank <58597814+canontank@users.noreply.github.com>
2023-11-24 14:51:04 +0900

    Add files via upload

--- giftcard/js/giftcard.js
+++ giftcard/js/giftcard.js
@@ -10,39 +10,6 @@ var cashKeyList = new Array('원가', '단가', '수량');
 
 var notiTitleArray = new Array('구분', '내용');
 // common/js/cardData.js 참고
-/*
-var cardValueArray = new Array(
-    new Array('텔로', '제약 없음 (실적용)'),
-    new Array('데일리', '일일 20~25만 결제'),
-    new Array('딥에코', '티몬, 위메프, 쿠팡, 지마켓, 옥션, 11번가'),
-    new Array('욜로', '티몬, 위메프, 쿠팡'),
-    new Array('SKT', '제약 없음 (실적용)'),
-    new Array('KT NU', '제약 없음 (실적용)'),
-    new Array('LG NU', '제약 없음 (실적용)'),
-    new Array('스마트애니', '스마트로, 이니시스, KICC'),
-    new Array('위클리', '주말(토/일) 2% 할인 (평일 실적 필요)'),
-    new Array('롯데국행', '지마켓, 옥션, 인터파크 (월 2회, 건당 10만)'),
-    new Array('딥온', '간편결제 (신한플레이, 스마일페이, SK페이)'),
-    new Array('주거래', '티몬, 위메프, 쿠팡, 지마켓, 옥션, 11번가'),
-    new Array('4tune', '티몬, 위메프, 쿠팡, 지마켓, 옥션, 11번가'),
-    new Array('쿠키', '지마켓, 11번가'),
-    new Array('쿠키런', '지마켓, 11번가'),
-    new Array('람다람', '지마켓, 11번가'),
-    new Array('총몇명', '지마켓, 11번가'),
-    new Array('야코', '지마켓, 11번가'),
-    new Array('스무살', '지마켓, 11번가'),
-    new Array('선불교통', '지마켓, 11번가'),
-    new Array('12세후불', '지마켓, 11번가'),
-    new Array('한베가족', '지마켓, 옥션 (일 1회, 월 2회)'),
-    new Array('롤챔코', '지마켓, 옥션 (일 1회, 월 2회)'),
-    new Array('더나은', '제약 없음 (실적용)'),
-    new Array('스마트캐시백', '건당 50만 이상 결제'),
-    new Array('010pay', '혜택 없음'),
-    new Array('K-First', '혜택 없음'),
-    new Array('농협국행', '혜택 없음'),
-    new Array('페이코', '제약 없음 (1% 적립)'),
-);
-*/
 
 var chargeTitleArray = new Array('구분', '해피머니', '북앤라이프', '컬쳐랜드', '문화상품권', 'SSG');
 var chargeLimitArray = new Array(
