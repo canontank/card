ed2bcb0f0ac44f16eafbb3c9125ba7fe4b7a9940
canontank <58597814+canontank@users.noreply.github.com>
2024-02-15 16:56:25 +0900

    Update cardData.js

--- common/js/cardData.js
+++ common/js/cardData.js
@@ -44,4 +44,4 @@ var cardValueArray = new Array(
 
 var cardArray1 = new Array('쿠키', '쿠키런', '람다람', '총몇명', '야코', '스무살', '선불교통', '12세후불');
 var cardArray2 = new Array('한베가족', '롤챔코');
-var cardArray3 = new Array('롯데국행');
+var cardArray3 = new Array('국민행복');
