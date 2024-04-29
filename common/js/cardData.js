5d227204a77a97082904555a33a4205d37f149bf
mskim <canontank@gmail.com>
2024-04-29 18:18:57 +0900

    K-패스 카드 추가

--- common/js/cardData.js
+++ common/js/cardData.js
@@ -1,3 +1,3 @@
 var cardArray1 = new Array('쿠키', '쿠키런', '람다람', '총몇명', '야코', '스무살', '선불교통', '12세후불');
-var cardArray2 = new Array('한베가족', '롤챔코');
+var cardArray2 = new Array('한베가족', '롤챔코', 'K-패스');
 var cardArray3 = new Array('국민행복');
